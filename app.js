f5a63ac3d3fdb60d095b286b15ee231f28ceb4e2
luc.nguyen@ut.edu.vn <Quocluc1997>
2018-11-04 22:19:23 +0700

    t met

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    response = receiveText(received_message.text);
+    response = receiveText(received_message);
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
